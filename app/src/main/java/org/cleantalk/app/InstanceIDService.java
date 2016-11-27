5adc28e0d59f0c64d18a6c94bb0788cacae4532d
Fridon Danelian <fedin.mail@gmail.com>
2016-11-27 03:44:39 +0300

    Added FCM support

---
+++ app/src/main/java/org/cleantalk/app/InstanceIDService.java
@@ -0,0 +1,28 @@
+package org.cleantalk.app;
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class InstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "FCM";
+
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String refreshedToken) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("FCM_TOKEN", refreshedToken);
+        editor.apply();
+    }
+
+}
---
+++ app/src/main/java/org/cleantalk/app/MessagingService.java
@@ -0,0 +1,21 @@
+package org.cleantalk.app;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "FCM";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // TODO(developer): Handle FCM messages here.
+        // If the application is in the foreground handle both data and notification messages here.
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+    }
+}
