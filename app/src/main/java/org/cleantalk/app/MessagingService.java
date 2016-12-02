edc9c553d7f911b1c0d2cdb4a00e9a61c0611ec9
Fridon Danelian <fedin.mail@gmail.com>
2016-12-02 18:59:38 +0300

    Fix for text and title

--- app/src/main/java/org/cleantalk/app/MessagingService.java
+++ app/src/main/java/org/cleantalk/app/MessagingService.java
@@ -15,28 +15,23 @@ import org.cleantalk.app.activities.MainActivity;
 
 public class MessagingService extends FirebaseMessagingService {
 
-    private static final String TAG = "FCM";
     public static final String ACTION_UPDATE = "ACTION_UPDATE";
+
+    private static final String TAG = "FCM";
     private static final int NOTIFICATION_ID = 1001;
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // TODO(developer): Handle FCM messages here.
-        // If the application is in the foreground handle both data and notification messages here.
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
         Log.d(TAG, "From: " + remoteMessage.getFrom());
-        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.getData().toString());
 
-        String title = remoteMessage.getNotification().getTitle();
-        String message = remoteMessage.getNotification().getBody();
-
+        String message = remoteMessage.getData().get("text");
+        String title = remoteMessage.getData().get("title");
         if (MainActivity.foreground) {
             LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(new Intent(ACTION_UPDATE));
         } else {
             showNotification(message, title);
         }
-
     }
 
     private void showNotification(String message, String title) {
